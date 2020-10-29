698b7b4b36f086640d4a5215f694ae5ab3eaec7c
Squanbri <Squan-bri@mail.ru>
2020-10-29 23:01:52 +0300

    realese v2 login and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'messages#index'
   resources :messages
+  devise_for :users
 end
