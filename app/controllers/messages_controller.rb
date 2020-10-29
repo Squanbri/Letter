1eb66d1bdc84e64fb440a1198aa2deb8d817ee1c
Squanbri <Squan-bri@mail.ru>
2020-10-29 19:44:37 +0300

    base

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,29 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(post_params)
+    if @message.save
+      redirect_to root_path
+    else
+      render new
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:message).permit(:company, :body)
+  end
+end
